5e150d72da4da3f18c85eea5da0b9413e9a19cc5
Harshal Bhavsar <154224487+Harshbhavsar2004@users.noreply.github.com>
2024-12-26 11:59:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://mittal-frontend-1.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
